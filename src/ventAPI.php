a70076c478ff41498d2e217e5112c7bce84aaf7d
Galileon-venta <48894231+Galileon-venta@users.noreply.github.com>
2020-10-26 18:51:40 +0100

    Created new Commands

--- src/ventAPI.php
+++ src/ventAPI.php
@@ -15,15 +15,14 @@ class ventAPI{
         $this->http = new GuzzleHttp\Client();
     }
 
-    public function file_get_contents_secure($url){
-        global $http;
-        return $http->request('GET',$url,[
-            'headers' => [
-                'AUTHORIZATION' => 'Bearer '.$_SESSION['ventAPItoken'],
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ]
-        ])->getBody();
+    public function ventAPI_get_game_groups(){
+        $url = "http://ventaGaming.de:9090/api/secures/groups/gamegroups/webpanel";
+        return json_decode($this->file_get_contents_secure($url), true);
+    }
+
+    public function ventAPI_get_random_feedback(){
+        $url = "http://ventaGaming.de:9090/api/secure/web/getFeedback/random";
+        return json_decode($this->file_get_contents_secure($url));
     }
 
     public function ventAPI_login($username,$password){
@@ -42,4 +41,14 @@ class ventAPI{
         $_SESSION['ventAPItoken'] = $response->getBody();
     }
 
+    private function file_get_contents_secure($url){
+        global $http;
+        return $http->request('GET',$url,[
+            'headers' => [
+                'AUTHORIZATION' => 'Bearer '.$_SESSION['ventAPItoken'],
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ]
+        ])->getBody();
+    }
 }
